3d6325e60636bffaa410dc010b4d27e238347774
roacevedo <racevedo@andreani.com>
2025-02-18 13:05:01 -0300

    config base

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+    }).catch((error) => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
+  });
+}
