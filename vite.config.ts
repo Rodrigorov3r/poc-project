a5b0cf37bb7d89f4b3c0ff0299d3dd8013fcf9e9
Antony Fagundez <afagundez@andreani.com>
2025-03-17 16:05:45 -0300

    fix: update

--- vite.config.ts
+++ vite.config.ts
@@ -20,35 +20,35 @@ export default defineConfig({
           "orientation": "any", 
         "screenshots": [
           {
-            "src": "/screenshots/desktop.png",
+            "src": "https://rodrigorov3r.github.io/poc-project/screenshots/desktop.png",
             "sizes": "1806x821",
             "type": "image/png",
             "form_factor": "wide"
           },
           {
-            "src": "/screenshots/mobile.png",
+            "src": "https://rodrigorov3r.github.io/poc-project/screenshots/mobile.png",
             "sizes": "462x762",
             "type": "image/png"
           }
           ],
           "icons":[
             {
-              "src":"/mi_andreani_192.png",
+              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
               "sizes":"192x192",
               "type":"image/png"
             },
             {
-              "src":"/mi_andreani_256.png",
+              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_256.png",
               "sizes":"256x256",
               "type":"image/png"
             },
             {
-              "src":"/mi_andreani_384.png",
+              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_384.png",
               "sizes":"384x384",
               "type":"image/png"
             },
             {
-              "src":"/mi_andreani_512.png",
+              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
               "sizes":"512x512",
               "type":"image/png"
             }
@@ -58,7 +58,34 @@ export default defineConfig({
           ],
           "edge_side_panel": {
             "preferred_width": 400
-          }
+          },
+          // @ts-expect-error the widgets its allowed
+          "widgets": [
+            {
+                "name": "HCM",
+                "description": "widget to control the HCM",
+                "tag": "hcm",
+                "template": "pwamp-template",
+                "ms_ac_template": "/hcm-card.json",
+                "type": "application/json",
+                "screenshots": [
+                    {
+                        "src": "https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
+                        "sizes": "512x512",
+                        "label": "The PWAmp mini-player widget"
+                    }
+                ],
+                "icons": [
+                  {
+                    "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
+                    "sizes":"192x192",
+                    "type":"image/png"
+                  }
+                ],
+                "auth": false,
+                "update": 86400
+            }
+        ]
       },
     }),
   ],
