7a0b8ac5cf2d25a79466ececa5b43aed001c9a03
Antony Fagundez <afagundez@andreani.com>
2025-03-17 21:28:16 -0300

    fix: update

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+self.addEventListener("widgetinstall", (event) => {
+    // The widget just got installed, render it using renderWidget.
+    // Pass the event.widget object to the function.
+    event.waitUntil(renderWidget(event.widget));
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(updateWidgets());
+});
+
+async function renderWidget(widget) {
+    // Get the template and data URLs from the widget definition.
+    const templateUrl = widget.definition.msAcTemplate;
+    const dataUrl = widget.definition.data;
+
+    // Fetch the template text and data.
+    const template = await (await fetch(templateUrl)).text();
+    const data = await (await fetch(dataUrl)).text();
+
+    // Render the widget with the template and data.
+    await self.widgets.updateByTag(widget.definition.tag, { template, data });
+}
+
+async function updateWidgets() {
+    // Get the widget that match the tag defined in the web app manifest.
+    const widget = await self.widgets.getByTag("pwamp");
+    if (!widget) {
+        return;
+    }
+
+    // Using the widget definition, get the template and data.
+    const template = await (await fetch(widget.definition.msAcTemplate)).text();
+    const data = await (await fetch(widget.definition.data)).text();
+
+    // Render the widget with the template and data.
+    await self.widgets.updateByTag(widget.definition.tag, { template, data });
+}
+
+self.addEventListener('widgetclick', (event) => {
+    switch (event.action) {
+        case 'previous-song':
+            // Application logic to play the previous song...
+            break;
+        case 'next-song':
+            // Application logic to play the next song...
+            break;
+    }
+});
+
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,92 +1,94 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: 'poc-project',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-          "name": "SPA-Fichadas-MiAndreani",
-          "theme_color": "#000000",
-          "background_color": "#ffffff",
-          "short_name": "SPA-Fichadas-MiAndreani",
-          "id": "SPA-Fichadas-MiAndreani-PocWidget-",
-          "description": "POC Widget de Fichadas Mi Andreani",
-          "start_url": "/",
-          "display": "standalone",
-          "orientation": "any", 
-        "screenshots": [
-          {
-            "src": "https://rodrigorov3r.github.io/poc-project/screenshots/desktop.png",
-            "sizes": "1806x821",
-            "type": "image/png",
-            "form_factor": "wide"
-          },
-          {
-            "src": "https://rodrigorov3r.github.io/poc-project/screenshots/mobile.png",
-            "sizes": "462x762",
-            "type": "image/png"
-          }
-          ],
-          "icons":[
-            {
-              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
-              "sizes":"192x192",
-              "type":"image/png"
-            },
-            {
-              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_256.png",
-              "sizes":"256x256",
-              "type":"image/png"
-            },
-            {
-              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_384.png",
-              "sizes":"384x384",
-              "type":"image/png"
-            },
-            {
-              "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
-              "sizes":"512x512",
-              "type":"image/png"
-            }
-          ],
-          "categories": [
-            "utilities"
-          ],
-          "edge_side_panel": {
-            "preferred_width": 400
-          },
-          // @ts-expect-error the widgets its allowed
-          "widgets": [
-            {
-                "name": "HCM",
-                "description": "widget to control the HCM",
-                "tag": "hcm",
-                "template": "pwamp-template",
-                "ms_ac_template": "/hcm-card.json",
-                "type": "application/json",
-                "screenshots": [
-                    {
-                        "src": "https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
-                        "sizes": "512x512",
-                        "label": "The PWAmp mini-player widget"
-                    }
-                ],
-                "icons": [
-                  {
-                    "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
-                    "sizes":"192x192",
-                    "type":"image/png"
-                  }
-                ],
-                "auth": false,
-                "update": 86400
-            }
-        ]
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   injectRegister: 'script',
+
+    //   manifest: {
+    //       "name": "SPA-Fichadas-MiAndreani",
+    //       "theme_color": "#000000",
+    //       "background_color": "#ffffff",
+    //       "short_name": "SPA-Fichadas-MiAndreani",
+    //       "id": "SPA-Fichadas-MiAndreani-PocWidget-",
+    //       "description": "POC Widget de Fichadas Mi Andreani",
+    //       "start_url": "/",
+    //       "display": "standalone",
+    //       "orientation": "any", 
+    //     "screenshots": [
+    //       {
+    //         "src": "https://rodrigorov3r.github.io/poc-project/screenshots/desktop.png",
+    //         "sizes": "1806x821",
+    //         "type": "image/png",
+    //         "form_factor": "wide"
+    //       },
+    //       {
+    //         "src": "https://rodrigorov3r.github.io/poc-project/screenshots/mobile.png",
+    //         "sizes": "462x762",
+    //         "type": "image/png"
+    //       }
+    //       ],
+    //       "icons":[
+    //         {
+    //           "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
+    //           "sizes":"192x192",
+    //           "type":"image/png"
+    //         },
+    //         {
+    //           "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_256.png",
+    //           "sizes":"256x256",
+    //           "type":"image/png"
+    //         },
+    //         {
+    //           "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_384.png",
+    //           "sizes":"384x384",
+    //           "type":"image/png"
+    //         },
+    //         {
+    //           "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
+    //           "sizes":"512x512",
+    //           "type":"image/png"
+    //         }
+    //       ],
+    //       "categories": [
+    //         "utilities"
+    //       ],
+    //       "edge_side_panel": {
+    //         "preferred_width": 400
+    //       },
+    //       // @ts-expect-error the widgets its allowed
+    //       "widgets": [
+    //         {
+    //             "name": "HCM",
+    //             "description": "widget to control the HCM",
+    //             "tag": "hcm",
+    //             "template": "pwamp-template",
+    //             "ms_ac_template": "https://rodrigorov3r.github.io/poc-project/hcm-card.json",
+    //             "type": "application/json",
+    //             "screenshots": [
+    //                 {
+    //                     "src": "https://rodrigorov3r.github.io/poc-project/mi_andreani_512.png",
+    //                     "sizes": "512x512",
+    //                     "label": "The PWAmp mini-player widget"
+    //                 }
+    //             ],
+    //             "icons": [
+    //               {
+    //                 "src":"https://rodrigorov3r.github.io/poc-project/mi_andreani_192.png",
+    //                 "sizes":"192x192",
+    //                 "type":"image/png"
+    //               }
+    //             ],
+    //             "auth": false,
+    //             "update": 86400
+    //         }
+    //     ]
+    //   },
+    // }),
   ],
 });
